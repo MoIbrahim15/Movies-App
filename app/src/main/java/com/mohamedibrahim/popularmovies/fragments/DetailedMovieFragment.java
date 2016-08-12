dccc6f402a1092309c6954ac9b2c6cef0ef8960e
Mohamedibrahim1 <mido1_2007@hotmail.com>
2016-08-13 00:23:05 +0200

    data filled in detailed fragment

--- app/src/main/java/com/mohamedibrahim/popularmovies/fragments/DetailedMovieFragment.java
+++ app/src/main/java/com/mohamedibrahim/popularmovies/fragments/DetailedMovieFragment.java
@@ -6,13 +6,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.mohamedibrahim.popularmovies.R;
 import com.mohamedibrahim.popularmovies.models.Movie;
+import com.squareup.picasso.Picasso;
 
 
 public class DetailedMovieFragment extends Fragment {
     private Movie selectedMovie;
+
+    final static String BASE_POSTER_PATH = "http://image.tmdb.org/t/p/";
+    final static String SIZE = "w185";
+
 
     public DetailedMovieFragment() {
     }
@@ -34,6 +41,23 @@ public class DetailedMovieFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View mView = inflater.inflate(R.layout.fragment_detailed_movie, container, false);
+        TextView titleView = (TextView) mView.findViewById(R.id.title_label);
+        TextView date_view = (TextView) mView.findViewById(R.id.date_label);
+        TextView rateView = (TextView) mView.findViewById(R.id.rate_label);
+        TextView descView = (TextView) mView.findViewById(R.id.desc_label);
+        ImageView posterView = (ImageView) mView.findViewById(R.id.poster_img_detailed);
+
+        //fill data
+        if (selectedMovie != null) {
+            titleView.setText(selectedMovie.getTitle());
+            date_view.setText(selectedMovie.getReleaseDate());
+            rateView.setText(selectedMovie.getVoteAverage().toString() + "/10");
+            descView.setText(selectedMovie.getOverview());
+
+            String POSTER_PATH = selectedMovie.getPosterPath();
+            String FullPosterPath = BASE_POSTER_PATH + SIZE + POSTER_PATH;
+            Picasso.with(getContext()).load(FullPosterPath).into(posterView);
+        }
         return mView;
     }
 }
